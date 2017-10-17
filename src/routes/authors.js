3b23facda25b94262758c32a8b831178e11f4e99
ild <ild@dgroup.co>
2017-10-18 06:25:34 +0700

    add author routes

--- src/routes/authors.js
+++ src/routes/authors.js
@@ -13,7 +13,17 @@ router.get('/', (req, res) => {
 
 // TODO
 /* #1 GET request for getting info of an author. */
+/* #2 GET request for getting info of an author except nickname field. */
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const { nickname } = req.query;
 
-/* #2 GET request for getting info of an author except url field. */
+  const author = _.find(authors, { id: Number(id) });
 
+  if (nickname === 'true') {
+    res.json({ data: { author }});
+  } else {
+    res.json({ data: { author: _.omit(author, ['nickname']) }});
+  }
+});
 export default router;
