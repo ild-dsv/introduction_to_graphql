5c45859fa33c2e00ce16230aca33c8845fd27c2e
ild <ild@dgroup.co>
2017-10-18 07:05:57 +0700

    prepare for graphql start

--- src/server.js
+++ src/server.js
@@ -7,11 +7,6 @@ const app = express();
 const server = http.createServer(app);
 const PORT = 4000;
 
-//Import routes
-import index from './routes/index';
-import posts from './routes/posts';
-import authors from './routes/authors';
-
 // Enable cors
 app.use(cors());
 // parse application/x-www-form-urlencoded
@@ -19,9 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use('/', index);
-app.use('/posts', posts);     //Add posts routes to middleware chain
-app.use('/authors', authors); //Add authors routes to middleware chain
 
 server.listen(PORT, (err) => {
   if (err) {
